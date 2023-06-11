ee3a9085e54af162bad2da2bda99765e1045618e
Tanvir Ahmad <tanvirahmadstudent@gmail.com>
2023-06-11 23:16:48 +0600

    added Enrolled Classes component

--- src/pages/Dashboard/StudentDashboard/EnrolledClasses/EnrolledClasses.jsx
+++ src/pages/Dashboard/StudentDashboard/EnrolledClasses/EnrolledClasses.jsx
@@ -2,16 +2,19 @@ import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../../hooks/useAuth";
 import useUserSecure from "../../../../hooks/useUserSecure";
 
+
 const EnrolledClasses = () => {
     const [userSecure] = useUserSecure();
     const { user } = useAuth();
-    const { data: selectedclasses = [] } = useQuery(['selectedclasses'], async () => {
-        const res = await userSecure.get(`user/payments?email=${user.email}`);
+    const { data: enrolledclasses = [] } = useQuery(['enrolledclasses'], async () => {
+        const res = await userSecure.get(`user/enrolledclasses?email=${user?.email}`);
         return res.data;
     })
+  
+    console.log(user?.email,enrolledclasses)
     return (
         <div>
-        <h1 className="text-3xl font-semibold ">My Selected Classes</h1>
+        <h1 className="text-3xl font-semibold ">My Enrolled Classes</h1>
         <div className="overflow-x-auto">
             <table className="table table-zebra w-full">
                 {/* head */}
@@ -21,17 +24,19 @@ const EnrolledClasses = () => {
                         <th>Class Name</th>
                         <th>Image</th>
                         <th>Price</th>
-                      
+                        <th>Instructor Name</th>
+                       
                     </tr>
                 </thead>
                 <tbody>
                     {
-                        selectedclasses.map((classData, index) => <tr key={classData._id}>
+                        enrolledclasses.map((classData, index) => <tr key={classData._id}>
                             <th>{index + 1}</th>
-                            <td>{classData.className}</td>
-                            <td><img className="w-24 h-20" src={classData.image} alt="" /></td>
+                            <td>{classData.name}</td>
+                            <td><img src={classData.image} className="w-60 h-40" alt="" /></td>
                             <td>${classData.price}</td>
-                            <td>{classData.seats}</td>
+                            <td className="text-green-500 font-medium">{classData.instructorName}</td>
+                           
                            
                         </tr>)
                     }
